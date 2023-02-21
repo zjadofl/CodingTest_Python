16dddd5592aa37ae3ed948f67e4afb1cf6f3fdce
zjadofl <103254090+zjadofl@users.noreply.github.com>
2023-02-21 21:48:17 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,13 +1,10 @@
-n = input()
+n = int(input())
 cnt = 0
-for i in range(1, int(n)+1):
-    array = list(map(int, list(str(i))))
-    diff_array = []
-    if len(array) >= 2:
-        for j in range(len(array)-1):
-            diff_array.append(array[j+1] - array[j])
-        if diff_array.count(diff_array[0]) == len(diff_array):
-            cnt += 1
-    else:
+for i in range(1, n+1):
+    if i <= 99:
         cnt += 1
+    else:
+        array = list(map(int, str(i)))
+        if array[0] - array[1] == array[1] - array[2]:
+            cnt += 1
 print(cnt)
